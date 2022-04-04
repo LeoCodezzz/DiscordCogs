d808660ebb1c1fddf9ebe9933421a99435077046
Leo <102879121+LeoCodezzz@users.noreply.github.com>
2022-04-04 21:16:04 +0100

    Create TestCog.py

---
+++ TestCog.py
@@ -0,0 +1,34 @@
+#imports
+import discord
+from discord.ext import commands
+
+# CLASS FUNCTION
+class TestCog(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    
+    
+  @commands.command()
+  async def test(self, ctx): #ALWAYS CARRY "SELF" AS THE FIRST ARGUMENT!
+    await ctx.send("Congrats!, you have learnt to make a working cog!")
+    return
+  
+  
+  
+  
+  #SETUP FUNCTION
+  def setup(bot):
+    bot.add_cog(TestCog(bot)
+                
+  # YOU HAVE MADE A WORKING COG!
+  
+  # ALL COMMANDS MUST HAVE THE SAME INDENT SPACE AS THE "DEF" FUNCTION IN THE CLASS FUNCTION. EXAMPLE:
+                
+  class TestCog(commands.Cog):
+#---->     def __init__(self, bot):
+       self.bot = bot
+                
+  #SAME INDENT SPACE (arrows for example)              
+                
+#---->     @commands.command()
+           #rest of your command
